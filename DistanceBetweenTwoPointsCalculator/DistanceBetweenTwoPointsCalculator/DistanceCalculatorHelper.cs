39b64a169844add12c512d6cef1133f61388e2a6
Marcin Cwen <marcincwen@gmail.com>
2019-04-08 21:55:08 +0200

    obliczanie odległości między dwoma punktami w 2D

---
+++ DistanceBetweenTwoPointsCalculator/DistanceBetweenTwoPointsCalculator/DistanceCalculatorHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceBetweenTwoPointsCalculator
+{
+    class DistanceCalculatorHelper
+    {
+        static internal double DistanceBetweenTwoPoinst2D(double x1, double y1, double x2, double y2)
+        {
+            double temp1 = Math.Pow((x2 - x1), 2);
+            double temp2 = Math.Pow((y2 - y1), 2);
+            double result = Math.Sqrt(temp1 + temp2);
+            return result;
+        }
+
+    }
+}
---
+++ DistanceBetweenTwoPointsCalculator/DistanceBetweenTwoPointsCalculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceBetweenTwoPointsCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj współrzędne punktu pierwszego:");
+            Console.Write("x1 = ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y1 = ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Podaj współrzędne punktu drugiego:");
+            Console.Write("x2 = ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y2 = ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double distanceBetweenTwoPoints2D = DistanceCalculatorHelper.DistanceBetweenTwoPoinst2D(x1, y1, x2, y2);
+            Console.WriteLine("Odległość pomiędzy punktami wynosi: " + distanceBetweenTwoPoints2D);
+
+            Console.ReadKey();
+        }
+    }
+}
