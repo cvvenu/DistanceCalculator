a3a59fee058a441ba98d5a49b18e845461e26f88
Marcin Cwen <marcincwen@gmail.com>
2019-04-17 23:22:06 +0200

    dodanie obliczania odległości dla wymiaru 3D

--- DistanceBetweenTwoPointsCalculator/DistanceBetweenTwoPointsCalculator/DistanceCalculatorHelper.cs
+++ DistanceBetweenTwoPointsCalculator/DistanceBetweenTwoPointsCalculator/DistanceCalculatorHelper.cs
@@ -8,11 +8,20 @@ namespace DistanceBetweenTwoPointsCalculator
 {
     internal static class DistanceCalculatorHelper
     {
-        static internal double DistanceBetweenTwoPoinst2D(double x1, double y1, double x2, double y2)
+        static internal double DistanceBetweenTwoPoints2D(double x1, double y1, double x2, double y2)
         {
             double temp1 = Math.Pow((x2 - x1), 2);
             double temp2 = Math.Pow((y2 - y1), 2);
             double result = Math.Sqrt(temp1 + temp2);
+            return result;
+        }
+
+        static internal double DistanceBetweenTwoPoints3D(double x1, double y1, double z1, double x2, double y2, double z2)
+        {
+            double temp1 = Math.Pow((x1 - x2), 2);
+            double temp2 = Math.Pow((y1 - y2), 2);
+            double temp3 = Math.Pow((z1 - z2), 2);
+            double result = Math.Sqrt(temp1 + temp2 + temp3);
             return result;
         }
 
